0879a24478ffa8007b281927bbd9eac3a6d8dba0
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-04-23 11:09:24 -0400

    using translation file for navigation links

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,3 +1,7 @@
+<script setup>
+    import translations from '../config/nl-NL.js'
+</script>
+
 <template>
     <div class="navbar navbar-expand zapp-gradient fixed-top">
         <RouterLink to="/"><img class="navbar-brand ms-2 me-5" src="../assets/small-logo.png" height="50"/></RouterLink>
@@ -5,9 +9,9 @@
             {{$route.meta.title}}
         </div>
         <ul class="navbar-nav flex-row ms-auto d-md-flex float-right me-5">
-            <RouterLink class="nav-item nav-link text-white me-2" to="/">Medewerkers</RouterLink>
-            <RouterLink class="nav-item nav-link text-white me-2" to="/">Klanten</RouterLink>
-            <RouterLink class="nav-item nav-link text-white me-2" to="/login">Taken</RouterLink>
+            <RouterLink class="nav-item nav-link text-white me-2" :to="'/' + translations.employees">{{translations.employees}}</RouterLink>
+            <RouterLink class="nav-item nav-link text-white me-2" :to="'/' + translations.customers">{{translations.customers}}</RouterLink>
+            <RouterLink class="nav-item nav-link text-white me-2" :to="'/' + translations.tasks">{{translations.tasks}}</RouterLink>
         </ul>
     </div>
 </template>
