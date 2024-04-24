4e9eaf8af1cd0269d8c5758da8b0325f55492c24
MilanLucas <MilanLucas2001@outlook.com>
2024-04-24 14:32:17 +0200

    Cleaning imports

--- backend/src/main/java/nu/zapp/models/TaskModel.java
+++ backend/src/main/java/nu/zapp/models/TaskModel.java
@@ -2,12 +2,7 @@ package nu.zapp.models;
 
 import nu.zapp.entities.GeneralTasks;
 import nu.zapp.services.Crud;
-import org.aspectj.weaver.loadtime.Agent;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
