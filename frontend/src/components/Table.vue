6b2f690a466779ae61b766ec0f41e07de66272fe
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-04-25 04:14:00 -0400

    Table now gets data from property passed in by component that calls it

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,28 +1,19 @@
 <script setup>
     import { ref, watch } from "vue"
     import { useRoute } from "vue-router"
-
-    //var route = useRoute()
-    //watch(() => route.params.type, (newType, oldType) => {
-    //    // react to route changes...
-    //})
-
-    const dummy_list = ref([
-        { name: 'item 1', email: 'email@email.com' },
-        { name: 'item 2', email: 'email@email.com' },
-        { name: 'item 3', email: 'email@email.com' }
-    ]);
+    const props = defineProps(['list'])
+    console.log(props.list)
 </script>
 
 <template>
     <table class="table table-hover w-75">
         <thead>
             <tr>
-                <th v-for="key in Object.keys(dummy_list[0])">{{key}}</th>
+                <th v-for="key in Object.keys(props.list[0])">{{key}}</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="item in dummy_list">
+            <tr v-for="item in props.list">
                 <td class="px-0" v-for="value in Object.values(item)"><RouterLink to="/login">{{value}}</RouterLink></td>
             </tr>
         </tbody>
