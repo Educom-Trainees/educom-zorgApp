268c0d4b985aed73d9fe97734dfcb5e1f8b87ecc
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-04-25 04:15:16 -0400

    added vue query to app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from 'vue-query'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
