67cff1e713128f49afd3bcfc28afc6d9fa560923
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-04-26 10:23:40 -0400

    added more words to the translations file

--- frontend/src/config/nl-NL.js
+++ frontend/src/config/nl-NL.js
@@ -10,10 +10,25 @@ const translations = {
 	add_task: "Taak Toevoegen",
 	add_employee: "Medewerker Toevoegen",
 	add_customer: "Klant Toevoegen",
-	username: "Gebruikersnaam",
+	userName: "Gebruikersnaam",
 	password: "Wachtwoord",
 	login_button : "Inloggen",
 	forgot_password: "Vergeten",
+	search: "Zoek",
+	no_results: "Geen Resultaten",
+	id: "ID",
+	firstName: "Voornaam",
+	lastName: "Achternaam",
+	address: "Adres",
+	postalCode: "Postcode",
+	residence: "Woonplaats",
+	role: "Rol",
+	monday: "Maandag",
+	tuesday: "Dinsdag",
+	wednesday: "Woensdag",
+	thursday: "Donderdag",
+	friday: "Vrijdag",
+
 }
 
 export default translations;
