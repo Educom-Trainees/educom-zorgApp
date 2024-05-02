98f21a3fa54791bf2fec8b71951a7e60b9888b8e
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-05-02 08:57:37 -0400

    update CustomerItem

--- frontend/src/components/CustomerItem.vue
+++ frontend/src/components/CustomerItem.vue
@@ -1,17 +1,13 @@
 <template>
     <RouterLink to="../custdetail">
-        <div class="row ms-2">
-            <div class="col-10 name-address">
+        <div class="row ms-2 align-items-center">
+            <div class="col-10">
                 <div class="zapp-gradient circle-initials">
                     AA
                 </div>
-                <div class="row">
-                    <div class="col-12">
-                        name
-                    </div>
-                    <div class="col-12">
-                        address
-                    </div>
+                <div class="row d-inline-flex ms-1 h-100">
+                    <div class=" ">name</div>
+                    <div class=" ">address</div>
                 </div>
             </div>
             <div class="col-2 check-or-in-progress">
