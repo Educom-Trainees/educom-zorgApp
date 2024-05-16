d53bf21748f29cfd6aca43cba30ffbd29f6876c4
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-05-16 04:26:06 -0400

    changed AppModal to be a large modal instead

--- frontend/src/components/AppModal.vue
+++ frontend/src/components/AppModal.vue
@@ -25,7 +25,7 @@
 
     <!-- Modal -->
     <div class="modal fade" id="appModal" tabindex="-1" aria-labelledby="appModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="appModalLabel">{{props.title}}</h5>
