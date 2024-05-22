ae65e675add828feac3a6a83f773c586471a2983
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-05-22 05:52:29 -0400

    changed user icon and dropdown menu fixed positioning and added gradient (initial concept)

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -11,12 +11,12 @@
         <button class="navbar-toggler flex-row ms-auto float-right me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon" style = "color:white;"></span>
         </button>
-        <div class="dropdown order-md-last order-0">
-            <i class="fs-2 bi bi-person py-0 text-white me-3" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
+        <div class="dropdown order-md-last order-0 user-icon text-center">
+            <i class="fs-2 bi bi-person py-0 text-white h-100" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+            <div class="dropdown-menu dropdown-menu-end user-menu zapp-gradient-continue" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item text-white" href="#">Action</a>
+                <a class="dropdown-item text-white" href="#">Another action</a>
+                <a class="dropdown-item text-white" href="#">Something else here</a>
             </div>
         </div>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -28,3 +28,21 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+    .user-menu {
+        top: 0;
+        margin-top: 0.5rem !important;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border: 1px solid white;
+        border-top: none;
+        border-right: none;
+
+    }
+
+    .user-icon {
+        height: 50px;
+        width: 50px;
+    }
+</style>
