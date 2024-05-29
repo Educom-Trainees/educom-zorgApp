63b173db3d29fc1bf0d1d24cd4383b450d79d30b
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-05-29 06:24:38 -0400

    Calendar now pulls appoinments from db and links to appointment details

--- frontend/src/components/WeekCalendar.vue
+++ frontend/src/components/WeekCalendar.vue
@@ -1,11 +1,12 @@
 <script setup>
-    import { ref } from "vue"
+    import { ref, watch } from "vue"
     import { QueryClient, useQuery, useQueryClient } from 'vue-query';
     import translations from '../config/nl-NL'
     import { getAppointmentsForAWeek } from "../api/appointments";
-    
+    import { getCollection } from "../api/collections";
+    //import { useBreakpoints } from '../utils/WindowWidth'
 
-
+    //const { width, type } = useBreakpoints()
 
     const week = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
     const page = ref(0)
@@ -27,31 +28,56 @@
     return newDate;
 }
 
-    const firstDayOfCurrentWeek = firstDayOfWeek(new Date())
-    
-    const info = [
-        { name: "mr. guy", address: "real street 1, AB12345" },
-        { name: "mr. guy", address: "real street 1, AB12345" },
-        { name: "mr. guy", address: "real street 1, AB12345" },
-        { name: "mr. guy", address: "real street 1, AB12345" }
-    ]
+    const firstDayOfCurrentWeek = ref(addDays(firstDayOfWeek(new Date()), page.value * 7))
 
-    const fetchWeek = () => {
-        const day = addDays(firstDayOfCurrentWeek, page * 7);
-        const d = getAppointmentsForAWeek(day);
-        return d;
+    const APPOINTMENTS = 'appointments'
+
+    const queryClient = useQueryClient();
+    const { isLoading, isError, data } = useQuery({
+        queryKey: [APPOINTMENTS, firstDayOfCurrentWeek],
+        queryFn: () => getCollection(APPOINTMENTS, [
+            { param: 'date', value: firstDayOfCurrentWeek.value.toISOString().split('T')[0] },
+            { param: 'count', value: 7 }
+        ]), //might want to move customers to route meta info
+        placeholderData: () => {
+            // Use the smaller/list version of the customer from the CUSTOMERS
+            // query as the placeholder data for this customer query
+            const placeholder = queryClient
+                .getQueryData([APPOINTMENTS])
+            return placeholder ?? []
+        }
+    })
+
+    const appointments = ref([])
+
+    function updateAppointments(value) {
+        const unsortedAppointments = [...value]
+        const appointmentsByDay = [...Array(7).keys()].map((value) => {
+            return { day: value, appointments: [] }
+        })
+        unsortedAppointments.forEach((app) => {
+            const appointmentdate = new Date(app.date)
+            appointmentdate.setMinutes(appointmentdate.getMinutes() + appointmentdate.getTimezoneOffset())
+            const datediff = Math.floor((appointmentdate - firstDayOfCurrentWeek.value) / (1000 * 60 * 60 * 24))
+            appointmentsByDay.find(abd => abd.day == datediff).appointments.push(app)
+        })
+        appointments.value = appointmentsByDay
     }
 
-    const { isLoading, isError, data, error, isFetching } = useQuery({
-        queryKey: ['calendar', page],
-        queryFn: fetchWeek,
-    })
+    if (!isLoading.value && data.value) {
+        updateAppointments(data.value)
+    }
+
+    watch(data, (value) => updateAppointments(value));
 
     const prevPage = () => {
         page.value--
+        firstDayOfCurrentWeek.value = addDays(firstDayOfWeek(new Date()), page.value * 7)
+
     }
     const nextPage = () => {
         page.value++
+        firstDayOfCurrentWeek.value = addDays(firstDayOfWeek(new Date()), page.value * 7)
     }
 </script>
 
@@ -71,7 +97,7 @@
             <div class="col-8 col-md-10">
                 <div class="row">
 
-                    <template v-for="item in data">
+                    <template v-for="item in appointments">
                         <div class="day col-12 col-md-3 col-lg zapp-gradient">
                             <div class="day-title d-flex justify-content-between mb-2">
                                 <span class="float-left ms-1 text-white">
@@ -82,14 +108,17 @@
                                 </span>
                             </div>
                             <template class="" v-for="appointment in item.appointments">
-                                <div class="mb-2">
-                                    <div class="name-text text-white text-wrap">
-                                        {{appointment.name}}
+                                <RouterLink class="nav-item nav-link text-white" :to="'/' + translations.appointments + '/' + appointment.id">
+
+                                    <div class="mb-2">
+                                        <div class="name-text text-white text-wrap">
+                                            {{appointment.name}}
+                                        </div>
+                                        <div class="address-text text-white text-wrap">
+                                            {{appointment.location}}
+                                        </div>
                                     </div>
-                                    <div class="address-text text-white text-wrap">
-                                        {{appointment.address}}
-                                    </div>
-                                </div>
+                                </RouterLink>
                             </template>
                         </div>
                     </template>
