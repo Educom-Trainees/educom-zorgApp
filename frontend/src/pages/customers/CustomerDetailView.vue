3745dddd483a692a4efadbb92663599f140b5406
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-05-29 06:20:58 -0400

    edited properties to align with changes in backend

--- frontend/src/pages/customers/CustomerDetailView.vue
+++ frontend/src/pages/customers/CustomerDetailView.vue
@@ -65,11 +65,11 @@
     <template v-else>
         <div class="row">
             <form class="offset-1 col-10" @submit.prevent="postIfValid">
-                <InputForm type="text" :label="translations.firstName" v-model="customer.firstName" id="firstName" />
+                <InputForm type="text" :label="translations.firstName" v-model="customer.name" id="name" />
                 <InputForm type="text" :label="translations.lastName" v-model="customer.lastName" id="lastName" />
                 <InputForm type="text" :label="translations.address" v-model="customer.address" id="address" />
                 <div class="row">
-                    <InputForm class="col-6" type="text" :label="translations.postalCode" v-model="customer.postalCode" id="postalCode" />
+                    <InputForm class="col-6" type="text" :label="translations.postalCode" v-model="customer.postalcode" id="postalcode" />
                     <InputForm class="col-6" type="text" :label="translations.residence" v-model="customer.residence" id="residence" />
                 </div>
                 <button type="submit" class="position-bottom-right default-button mb-4 me-4">{{translations.save}}</button>
