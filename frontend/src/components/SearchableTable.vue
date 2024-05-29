b8926bb5be73cf5f6aa1c0a893f256fd92effbb7
Thomas-van-Haastrecht <thomas.hjvh@gmail.com>
2024-05-29 12:21:24 -0400

    Fixed Listview to only show active items when not searching Removed certain fields from view (id, active, etc.) Changed css - prev and next buttons now stay in same place, and top of list is always at the top of the screen

--- frontend/src/components/SearchableTable.vue
+++ frontend/src/components/SearchableTable.vue
@@ -8,7 +8,7 @@
 
     const pageLength = 10; // number of items shown per page
 
-    const validData = ref(props.list);
+    const validData = ref(props.list.filter((item) => item.active));
     const searchval = ref('');
     const page = ref(1);
 
@@ -16,9 +16,12 @@
         page.value = 1;
     }
 
+    const excludeList = ['id', 'active', 'startTime', 'endTime', 'workSchedules']
+
     const filterList = () => {
         if (searchval.value.length == 0) {
-            validData.value = props.list;
+            validData.value = props.list.filter((item) => item.active);
+            console.log(validData.value)
             return;
         }
 
@@ -39,23 +42,23 @@
 </script>
 
 <template>
-    <div class="row justify-content-center mt-5">
+    <div class="row justify-content-center py-4">
         <input class="form-control w-50 bg-light" v-model="searchval" :placeholder="translations.search + ' ' + props.singular.toLowerCase()" @keyup="updateList" />
     </div>
-    <div class="d-flex justify-content-center align-items-center mt-3">
+    <div class="d-flex justify-content-center align-items-center table-container">
         <button class="prev-button me-5" @click="prevPage" :disabled="page == 1"></button>
 
         <div v-if="validData.length == 0" class="text-center w-75">{{translations.no_results}}</div>
         <template v-else>
-            <table class="table table-hover w-75">
+            <table class="table table-hover w-75 align-self-start">
                 <thead>
                     <tr>
-                        <th class="px-0" v-for="key in Object.keys(validData[0])">{{translations[key] ?? key}}</th>
+                        <th class="px-0" v-for="key in Object.keys(validData[0]).filter((key) => !excludeList.includes(key))">{{translations[key] ?? key}}</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="item in validData.slice((page-1)*pageLength, page*pageLength)">
-                        <td class="px-0" v-for="value in Object.values(item)"><RouterLink :to="'/'+translations[props.listType]+'/'+item.id">{{value}}</RouterLink></td>
+                    <tr v-for="item in validData.filter((key, value) => !excludeList.includes(key)).slice((page-1)*pageLength, page*pageLength)">
+                        <td class="px-0" v-for="keyValuePair in Object.entries(item).filter(keyValuePair => !excludeList.includes(keyValuePair[0]))"><RouterLink :to="'/'+translations[props.listType]+'/'+item.id">{{keyValuePair[1]}}</RouterLink></td>
                     </tr>
                 </tbody>
             </table>
@@ -63,5 +66,12 @@
 
         <button class="next-button ms-5" @click="nextPage" :disabled="page >= Math.ceil(validData.length/pageLength)"></button>
     </div>
+    <div class="my-5"></div>
 
 </template>
+
+<style scoped>
+    .table-container {
+        flex: 1 1 auto;
+    }
+</style>
