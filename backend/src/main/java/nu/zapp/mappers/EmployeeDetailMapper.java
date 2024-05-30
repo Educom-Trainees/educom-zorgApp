130c6b911b4c6e20c40efb4bd22e25b3ae5aacae
MilanLucas <MilanLucas2001@outlook.com>
2024-05-30 10:28:21 +0200

    Employee mapper imrpovement additional data

--- backend/src/main/java/nu/zapp/mappers/EmployeeDetailMapper.java
+++ backend/src/main/java/nu/zapp/mappers/EmployeeDetailMapper.java
@@ -5,12 +5,16 @@ import nu.zapp.entities.Employee;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.util.List;
 @Mapper(componentModel = "spring", uses = BaseMapper.class)
 public interface EmployeeDetailMapper extends BaseMapper {
 
+    @Mapping(target="firstName", source = "destination.name")
+    @Mapping(target="workSchedule", source ="destination.workSchedule")
+    Employee destinationToSource(EmployeeDetailDTO destination);
+
     @Mapping(target="name", expression="java(source.getFirstName())")
     @Mapping(target="lastName", expression="java(source.getLastName())")
+    @Mapping(target="workSchedule", expression = "java(source.getWorkSchedule())")
     EmployeeDetailDTO sourceToDestination(Employee source);
 
 }
