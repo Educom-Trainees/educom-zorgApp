f070ba908597d09315ac48599d4b2d81c0b0a5ff
MilanLucas <MilanLucas2001@outlook.com>
2024-05-30 10:30:44 +0200

    EmployeeRepo clean up

--- backend/src/main/java/nu/zapp/repositories/EmployeeRepository.java
+++ backend/src/main/java/nu/zapp/repositories/EmployeeRepository.java
@@ -8,11 +8,9 @@ import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-    //Both findByUsername and findById are for detail pages
 
     Employee findByUsername(String username);
 
-    //@Query("SELECT a FROM Employee a LEFT JOIN FETCH a.employeeToAppointments b LEFT JOIN FETCH b.appointment c")
     Employee findById(int id);
 
     List<Employee> findAll();
